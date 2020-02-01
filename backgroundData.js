9f336424c22be1d38ff9fe75739e9ba7bbeeea23
nourinjh <nourin.abdelhadi@gmail.com>
2020-02-01 17:13:25 -0500

    added nav bar

--- backgroundData.js
+++ backgroundData.js
@@ -1,37 +1,27 @@
-
-
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tabs) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        if (typeof tabs[0] !== 'undefined') {
-
-            var url = new URL(tabs[0].url)
-            title = tabs[0].title
-            console.log(`URL: ${url}`)
-            console.log(`Title: ${title}`)
-            var origin = url.origin;
-            console.log(origin);
-            var companyName = "";
-            var j =0;
-            var start = false;
-            var i = 8;
-            if(origin.includes("www.")){
-                var i = 12;
-            }
-            for(i; i<origin.length;i++){
-                
-                if(origin.charAt(i)=='.'){
-                    break;
-                }
-    
-                    companyName = companyName.concat(origin.charAt(i));
-                
-            }
-            console.log(companyName);
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    if (typeof tabs[0] !== "undefined") {
+      var url = new URL(tabs[0].url);
+      title = tabs[0].title;
+      console.log(`URL: ${url}`);
+      console.log(`Title: ${title}`);
+      var origin = url.origin;
+      console.log(origin);
+      var companyName = "";
+      var j = 0;
+      var start = false;
+      var i = 8;
+      if (origin.includes("www.")) {
+        var i = 12;
+      }
+      for (i; i < origin.length; i++) {
+        if (origin.charAt(i) == ".") {
+          break;
         }
-        
 
-    })
-
-
-
-})
+        companyName = companyName.concat(origin.charAt(i));
+      }
+      console.log(companyName);
+    }
+  });
+});
